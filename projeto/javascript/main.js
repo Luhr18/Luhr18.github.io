8368fad7bd969322bca75e40281948dcfa5a390a
Luhr18 <203120@upf.br>
2024-06-10 20:02:19 -0300

    Add files via upload

---
+++ projeto/javascript/main.js
@@ -0,0 +1,45 @@
+/* tabela */
+function deletarlinha1() {
+    document.getElementById("linha1").style.visibility = "hidden";
+}
+
+function deletarlinha2() {
+    document.getElementById("linha2").style.visibility = "hidden";
+}
+
+function deletarlinha3() {
+    document.getElementById("linha3").style.visibility = "hidden";
+}
+
+function deletarlinha4() {
+    document.getElementById("linha4").style.visibility = "hidden";
+}
+
+function deletarlinha5() {
+    document.getElementById("linha5").style.visibility = "hidden";
+}
+
+/* API */
+
+
+
+/* botões pra esconder */
+
+function mostrarDiv1() {
+    document.getElementById("div1").style.visibility = "visible";
+    document.getElementById("div2").style.visibility = "hidden";
+    document.getElementById("div3").style.visibility = "hidden";
+}
+
+function mostrarDiv2() {
+    document.getElementById("div2").style.visibility = "visible";
+    document.getElementById("div1").style.visibility = "hidden";
+    document.getElementById("div3").style.visibility = "hidden";
+}
+
+function mostrarDiv3() {
+    document.getElementById("div3").style.visibility = "visible";
+    document.getElementById("div1").style.visibility = "hidden";
+    document.getElementById("div2").style.visibility = "hidden";
+}
+
